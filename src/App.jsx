c1ceec1f6a9c81506aeb4de1db7e646407ae1d97
T1mek <misha1234567890m@gmail.com>
2023-06-12 12:33:50 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.scss';
 import Header from "./components/Header/Header";
 import {useTelegram} from "./hooks/useTelegram";
 import {useEffect} from "react";
-import ProductList from "./components/ProductList/ProductList";
+import ProductList from "./components/ProductList/ProductList.jsx";
 
 
 
@@ -17,7 +17,9 @@ function App() {
   return (
     <div className="App">
         <Header/>
+        <p>sddsfsdf</p>
         <ProductList/>
+        <p>dfsdfasdf</p>
     </div>
   );
 }
